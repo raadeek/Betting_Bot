f95347493f49f3f37b1b2e52339a58c1d312991c
Jan Jarzebinski <janjarzebinski1@gmail.com>
2026-03-18 10:23:47 +0100

    Added get_date

--- scraping.py
+++ scraping.py
@@ -1,24 +1,35 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+
+YEAR='2026'
 
 #gets url from a file (must be only one url in a file)
 def get_url(filename : str) -> str:
     with open(filename, 'r', encoding='utf-8') as f:
         return f.read()
         
-def get_response(url : str):
-    return requests.get(url)
 
 def get_soup(url : str) -> BeautifulSoup:
-    return BeautifulSoup(get_response(url).text, "html.parser")
+    response = requests.get(url)
+    return BeautifulSoup(response.text, "html.parser")
 
-#return dict with dates (strings) as keys, and list of match spacebars (QueryResults) as values
-def get_matches_dates(soup : BeautifulSoup) -> dict : 
+
+#return dict with dates (strings) as keys, and list of match spacebars (list[QueryResults]) as values
+def create_matches_dict(soup : BeautifulSoup) -> dict : 
     cardEvents = soup.find_all(class_='groupEvents')
     score_wrappers = [event.find_all(class_='scoreboard_wrapper') for event in cardEvents]
     dates=[event.find(class_='groupEvents_headTitle').text for event in cardEvents]
 
     return dict(zip(dates, score_wrappers))
+
+
+def get_date(data_str : str) -> datetime:       #data_str format 'xxx day/month'
+    date = data_str.split()
+    raw_date = date[1] + f'/{YEAR}'
+    mask = "%d/%m/%Y"
+
+    return datetime.strptime(raw_date, mask)
 
 
 def print_matches(data : dict):
