fae544aacf84ab6f21c7a9dd69371077877f674f
Jan Jarzebinski <janjarzebinski1@gmail.com>
2026-03-22 23:43:49 +0100

    Added bet and bet_event class

---
+++ bet.py
@@ -0,0 +1,7 @@
+from bet_event import BetEvent
+
+class Bet:
+    def __init__(self, bets : list[BetEvent]):
+        self.bets = bets
+
+
---
+++ bet_event.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+class BetEvent:
+    '''
+        Every single event player bets is stored as a BetEvent. Each Bet consist of multiple BetEvents.
+        betted - -1 if betted away_team, 0 if betted draw, 1 if betted home team
+    '''
+    def __init__(self, home : str, away : str, odds : float, betted : int, date : datetime):
+        self.home_team = home
+        self.away_team = away
+        self.odds = odds
+        self.date = date
+        self.betted = betted
+        self.betted_dict = {
+            -1 : self.away_team,
+            0 : "Draw",
+            1 : self.home_team,
+        }
+
+    def __str__(self):
+        return f"{self.home_team} - {self.away_team} --> {self.betted_dict.get(self.betted, "Wrong key in self.betted_dict{}")}: {self.odds}"       
+    
+
+    
