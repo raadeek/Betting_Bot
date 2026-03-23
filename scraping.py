eddbab279cb53f8c2b2f9c003543d27c79cf124c
Jan Jarzebinski <janjarzebinski1@gmail.com>
2026-03-24 00:11:53 +0100

    Added create_bet_event

--- scraping.py
+++ scraping.py
@@ -23,7 +23,7 @@ class Scraping:
             
     @staticmethod
     def get_soup(url : str) -> BeautifulSoup:
-        response = requests.get(url)
+        response = requests.get(url.strip())
         return BeautifulSoup(response.text, "html.parser")
 
 
@@ -34,7 +34,6 @@ class Scraping:
         #score_wrappers = [event.find_all(class_='scoreboard_wrapper') for event in cardEvents]
         match_event = [event.find_all(class_='cardEvent_content') for event in cardEvents]
         dates=[event.find(class_='groupEvents_headTitle').text for event in cardEvents]
-
         return dict(zip(dates, match_event))
     
 
@@ -44,16 +43,39 @@ class Scraping:
         date = data_str.split()
         raw_date = date[1] + f'/{YEAR}'
         mask = "%d/%m/%Y"
-
         return datetime.strptime(raw_date, mask)
+
 
     def get_match_from_team_name(self, team_name : str):               #TODO problem when one team has more than one match on the page
         data = self.matches_dict
         for i in range(len(data)):
             for j in list(data.values())[i]:
                 if team_name in j.text:
-                    print(j.text)
                     return j
+                
+    #used in create_bet_event
+    @staticmethod
+    def get_betted(teamname, home, away):
+        if teamname in home:
+            return 1
+        elif teamname in away:
+            return -1
+        else:
+            return 0
+
+    #creates dict with data needed for bet_event based on self.matches_dict. teamname - team which has been betted on
+    def create_bet_event(self, teamname : str) -> dict:
+        data = self.matches_dict
+        query = self.get_match_from_team_name(teamname)
+        bet_event = {
+            "home" : query.find(class_='scoreboard_contestant scoreboard_contestant-1').text,
+            "away" : query.find(class_='scoreboard_contestant scoreboard_contestant-2').text,
+            "odds" : [div.get_text(strip=True) for div in query.find_all(class_='btn_label') if div.get_text(strip=True)[0].isdigit()],             #TODO add date
+        }
+        
+        bet_event["betted"] = self.get_betted(teamname, bet_event['home'], bet_event['away'])
+
+        return bet_event
 
 
     def print_matches(self):
@@ -64,5 +86,6 @@ class Scraping:
                 print(f"      {j.text}")
 
 
-sc = Scraping('urls.txt')
 
+sc = Scraping('urls.txt')
+sc.create_bet_event("West")
