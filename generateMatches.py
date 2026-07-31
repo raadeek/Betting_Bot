3e3f83dff13255b1e66d1bfef54dfedc4837e619
Jan Jarzebinski <janjarzebinski1@gmail.com>
2026-07-31 02:30:13 +0200

    Added generating fictional matches to test app

---
+++ generateMatches.py
@@ -0,0 +1,85 @@
+import match, bet, random, json, datetime
+
+from pathlib import Path
+
+TEAMS = [
+    "Manchester City",
+    "FC Arsenal",
+    "Chelsea FC",
+    "FC Liverpool",
+    "Tottenham Hotspur",
+    "Manchester United",
+    "Brighton & Hove Albion",
+    "Newcastle United",
+    "Crystal Palace",
+    "AFC Bournemouth",
+    "FC Brentford",
+    "Aston Villa",
+    "Nottingham Forest",
+    "FC Everton",
+    "Leeds United",
+    "AFC Sunderland",
+    "FC Fulham",
+    "Ipswich Town",
+    "Coventry City",
+    "Hull City"
+]
+
+def generateTeams(data : list):
+    if data is None or len(data) < 2:
+        raise ValueError
+    
+    teams = random.sample(data, 2)
+    return teams[0], teams[1]
+
+def generateOdds():
+    homeOdd = random.uniform(1.2, 5.0)
+    awayOdd = 5.0 - homeOdd + 1.0
+    drawOdd = (homeOdd + awayOdd) / 2.0
+
+    homeOdd = round(homeOdd * random.uniform(0.9, 1.1), 2)
+    awayOdd = round(awayOdd * random.uniform(0.9, 1.1), 2)
+    drawOdd = round(drawOdd * random.uniform(0.9, 1.1), 2)
+
+    return homeOdd, awayOdd, drawOdd
+
+
+def generateDate(days: int):
+    dates = []
+    a = datetime.date.today()
+    for x in range(days):
+        dates.append(a + datetime.timedelta(days=x))
+    return random.choice(dates)
+
+
+def createMatch(teams_ : list):
+    home, away = generateTeams(teams_)
+    homeOdd, awayOdd, drawOdd = generateOdds()
+    date = generateDate(10)
+    date = str(date)
+  
+    match_ = match.Match(home, away, homeOdd, awayOdd, drawOdd, date)
+
+    return match_
+
+
+def generateTestMatches(teams_ : list, n : int):
+    matches = [createMatch(teams_) for i in range(n)]
+    return matches
+
+
+def generateJsonFile(filename : str):
+    a = Path(filename)
+
+    if not a.exists():
+        print("Filename not exists")
+        return
+
+    matches = generateTestMatches(TEAMS, 20)
+    dicts = [m.get_dict() for m in matches]
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(dicts, f, indent=4, ensure_ascii=False)
+
+
+generateJsonFile("matches.json")   
--- match.py
+++ match.py
@@ -2,11 +2,24 @@ import datetime
 
 
 class Match:
-    def __init__(self, home : str, away : str, home_odds : float, away_odds : float, draw_odds : float, date):
+    def __init__(self, home : str, away : str, home_odds : float, away_odds : float, draw_odds : float, date : str):
         self.home = home
         self.away = away
         self.home_odds = home_odds
         self.away_odds = away_odds
         self.draw_odds = draw_odds
         self.date = date
-        
+
+    def get_dict(self):
+        match_ = {
+            "home" : self.home,
+            "away" : self.away,
+            "homeOdds" : self.home_odds,
+            "drawOdds" : self.draw_odds,
+            "awayOdds" : self.away_odds,
+            "date" : self.date
+        }
+        return match_
+
+    def __str__(self):
+        return f"{self.home} - {self.away} odds: {self.home_odds} {self.draw_odds} {self.away_odds} Date: {self.date}"
